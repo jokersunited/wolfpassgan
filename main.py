30e01e47474fcabe28ca88dd52b440019d92a435
jshwwe <jshwwe@gmail.com>
2020-12-01 00:12:19 +0800

    finalized training

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from utils import *
 
 
 pass_len = 10
-good_list, charmap, inv_charmap = load_dataset("10-million-password-list-top-1000000.txt", pass_len)
+good_list, charmap, inv_charmap = load_dataset("../10-million-password-list-top-1000000.txt", pass_len)
 pass_shape = (len(inv_charmap), pass_len)
 
 noise_len = 100
@@ -22,10 +22,12 @@ def Conversion(one_dim):
     two_dim[one_dim[x]][x] = 1
   return two_dim
 
+
 #Generator Model
 def Generator():
   model = Sequential()
   model.add(Flatten(input_shape=noise_shape))
+  model.add(Dense(256))
   model.add(Dense(np.prod(pass_shape), activation='tanh'))
   model.add(Reshape(pass_shape))
   model.add(Softmax(axis=1))
@@ -39,6 +41,7 @@ def Generator():
 def Discriminator():
     model = Sequential()
     model.add(Conv1D(filters=5*pass_len, kernel_size=5, activation='relu', input_shape=(len(inv_charmap), 10)))
+    model.add(Dense(256))
     model.add(LeakyReLU(alpha=0.2))
     model.add(Flatten())
     model.add(Dense(1, activation='sigmoid'))
@@ -55,7 +58,6 @@ def save(epochs):
   output = generator.predict(noise)
   output = np.argmax(output, axis=1)
   with open("progress/" + str(epochs) + "_sample_100.txt", "w") as f:
-    print(output)
     for out in output:
       out_pass = ""
       for x in out:
@@ -64,7 +66,7 @@ def save(epochs):
 
 #The whole training process
 def training(epochs, batch_size, sample_interval):
-  for epoch in range (epochs):
+  for epoch in range (0, epochs):
     # Generate a half batch of fake images
     noise = np.random.normal(0, 1, (batch_size, noise_len))
     gen_imgs = generator.predict(noise)
@@ -80,7 +82,7 @@ def training(epochs, batch_size, sample_interval):
     #Take a random sample of the good batch to train
     idx = np.random.randint(0, len(good_list), batch_size)
     good_list_t = np.array(good_list)[idx]
-    good_list_t = [Conversion(x) for x in good_list]
+    good_list_t = np.array([Conversion(x) for x in good_list_t])
 
     #Train discriminator
     d_loss_real = discriminator.train_on_batch(good_list_t, np.ones((batch_size, 1)))
@@ -92,33 +94,33 @@ def training(epochs, batch_size, sample_interval):
     g_loss = combined.train_on_batch(noise, np.ones((batch_size, 1)))
 
     print("\n%d Iterations [D loss: %f, acc.: %.2f%%] [G loss: %f]" % (epoch, d_loss[0], 100*d_loss[1], g_loss))
-    if epochs % sample_interval == 0:
-      save(epochs)
+    if epoch % sample_interval == 0:
+      save(epoch)
 
-# #Things for ipynb
-# optimizer = Adam(0.0002, 0.5)
+#Things for ipynb
+optimizer = Adam(0.0002, 0.5)
 
-# generator = Generator()
-# generator.compile(loss='binary_crossentropy', optimizer=optimizer)
+generator = Generator()
+generator.compile(loss='binary_crossentropy', optimizer=optimizer)
 
-# discriminator = Discriminator()
-# discriminator.compile(loss='binary_crossentropy',
-#     optimizer=optimizer,
-#     metrics=['accuracy'])
+discriminator = Discriminator()
+discriminator.compile(loss='binary_crossentropy',
+    optimizer=optimizer,
+    metrics=['accuracy'])
 
-# #Defining input for combined model
-# z = Input(shape=noise_shape)
-# gen_pass = generator(z)
+#Defining input for combined model
+z = Input(shape=noise_shape)
+gen_pass = generator(z)
 
-# discriminator.trainable = False
+discriminator.trainable = False
 
-# #Pass output of generator to discriminator in the combined model
-# validity = discriminator(gen_pass)
+#Pass output of generator to discriminator in the combined model
+validity = discriminator(gen_pass)
 
-# #Finalize the combined model
-# combined = Model(z, validity)
-# combined.compile(loss='binary_crossentropy', optimizer=optimizer)
+#Finalize the combined model
+combined = Model(z, validity)
+combined.compile(loss='binary_crossentropy', optimizer=optimizer)
 
-# noise = np.random.normal(0, 1, (1, 100))
+noise = np.random.normal(0, 1, (1, 100))
 
-# training(100, 128, 50)
+training(100, 128, 25)
