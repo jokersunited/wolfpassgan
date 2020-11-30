61f4b1aec053e5a63dc3431b7d702bfa775985a1
jshwwe <jshwwe@gmail.com>
2020-11-30 13:27:23 +0800

    merged softmax layer into initial model

--- test.py
+++ test.py
@@ -19,6 +19,7 @@ def Generator():
   model.add(Dense(256))
   model.add(Dense(np.prod(pass_shape), activation='tanh'))
   model.add(Reshape(pass_shape))
+  model.add(Softmax(axis=1))
   model.summary()
 
   noise = Input(shape=noise_shape)
@@ -63,16 +64,16 @@ def softmax(logits, num_classes):
 print(gen_stuff)
 
 sample = np.argmax(gen_stuff, axis=1)
-sample2 = np.argmax(softmax(gen_stuff, 55), axis=1)
+# sample2 = np.argmax(softmax(gen_stuff, 55), axis=1)
 
 print(np.sum(gen_stuff, axis=1))
-print(np.sum(softmax(gen_stuff, 55), axis=1))
+# print(np.sum(softmax(gen_stuff, 55), axis=1))
 
 password = ""
 password2 = ""
 for x in sample[0]:
   password += chr(x+65)
 print(password)
-for x in sample2[0]:
-  password2 += chr(x+65)
-print(password2)
+# for x in sample2[0]:
+#   password2 += chr(x+65)
+# print(password2)
