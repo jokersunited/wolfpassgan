8bdeab21227f61cda2d017399e9ed57b0bef1778
mervynerkw <1802357@sit.singaporetech.edu.sg>
2020-12-01 00:09:07 +0800

    blocks structure

--- test.py
+++ test.py
@@ -1,7 +1,8 @@
 import tensorflow as tf
-from keras.layers import Input, Dense, Reshape, Flatten
+from keras.layers import Input, Dense, Reshape, Flatten, Conv1D, multiply, add
 from keras.layers import BatchNormalization
-from keras.layers.advanced_activations import LeakyReLU, Softmax
+from keras.activations import linear, relu
+from keras.layers.advanced_activations import LeakyReLU, ReLU, Softmax
 from keras.models import Sequential, Model
 import numpy as np
 
@@ -11,34 +12,70 @@ pass_shape = (charmap, pass_len)
 
 noise_shape = (100,)
 
+layer_dim = 1
+kernel_size = 55
+
+def ResBlock(model):
+    block = model
+    block.add(ReLU(10))
+    # block = relu(block)
+    block.add(Conv1D(filters=1, kernel_size=kernel_size, activation='relu', input_shape=(10, layer_dim), padding='same'))
+    block.add(ReLU(10))
+    # block = relu(block)
+    block.add(Conv1D(filters=1, kernel_size=kernel_size, activation='relu', input_shape=(10, layer_dim), padding='same'))
+    # return multiply([model, block])
+    return block
+
 #Generator Model
 
 def Generator():
-  model = Sequential()
-  model.add(Flatten(input_shape=noise_shape))
-  model.add(Dense(256))
-  model.add(Dense(np.prod(pass_shape), activation='tanh'))
-  model.add(Reshape(pass_shape))
-  model.add(Softmax(axis=1))
-  model.summary()
+    model = Sequential()
+    model.add(Flatten(input_shape=noise_shape))
+    # model.add(Dense(10, input_shape=noise_shape))
+    # model.add(Dense(256, input_shape=noise_shape))
+    # model.add(Dense(np.prod(pass_shape), activation='sigmoid'))
+    # model.add(Conv1D(filters=1, kernel_size=10, activation='relu', input_shape=(10, 1), padding='same'))
+    
+    model = linear(model)
 
-  noise = Input(shape=noise_shape)
-  passwords = model(noise)
+    model.add(Reshape((100, layer_dim)))
+    model = ResBlock(model)
+    model = ResBlock(model)
+    model = ResBlock(model)
+    model = ResBlock(model)
+    model = ResBlock(model)
 
-  return Model(noise, passwords)
+    model.add(Conv1D(filters=10, kernel_size=kernel_size, activation='relu', input_shape=(10, 1), padding='same'))
+    model.add(Softmax(axis=1))
+    model.summary()
+
+    noise = Input(shape=noise_shape)
+    passwords = model(noise)
+
+    return Model(noise, passwords)
 
 #Discriminator Model
 
 def Discriminator():
     model = Sequential()
-
-    model.add(Flatten(input_shape=pass_shape))
-    model.add(Dense(256))
-    model.add(LeakyReLU(alpha=0.2))
-    model.add(Dense(1, activation='sigmoid'))
+    
+    # model.add(Flatten(input_shape=pass_shape))
+    model.add(Conv1D(filters=1, kernel_size=10, activation='relu', input_shape=(10, 1), padding='same'))
+    model = ResBlock(model)
+    model = ResBlock(model)
+    model = ResBlock(model)
+    model = ResBlock(model)
+    model = ResBlock(model)
+    # model.add(Reshape((10, 1)))
+    model = linear(model)
+    
+    # model.add(Dense(256))
+    # model.add(LeakyReLU(alpha=0.2))
+    # model.add(Flatten())
+    # model.add(Dense(1, activation='sigmoid'))
     model.summary()
 
-    img = Input(shape=pass_shape)
+    img = Input(shape=(10, 1))
     validity = model(img)
 
     return Model(img, validity)
@@ -50,7 +87,9 @@ discriminator = Discriminator()
 noise = np.random.normal(0, 1, (1, 100))
 gen_stuff = generator.predict(noise)
 
-discriminate = discriminator.predict(gen_stuff)
+sample = np.argmax(gen_stuff, axis=1)
+
+discriminate = discriminator.predict(sample)
 print(discriminate)
 
 def softmax(logits, num_classes):
