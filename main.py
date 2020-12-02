c673666f513aa8cda1c284d6652200d8dc7285fd
jshww <jshwee@gmail.com>
2020-12-02 18:42:27 +0800

    YEAH

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import tensorflow as tf
-from keras.layers import Input, Dense, Reshape, Flatten, Conv1D, Multiply
+from keras.layers import Input, Dense, Reshape, Flatten, Conv1D, Multiply, Permute
 from keras.layers import BatchNormalization
 from keras.layers.advanced_activations import LeakyReLU, Softmax
 from keras.models import Sequential, Model
@@ -8,12 +8,12 @@ from keras.backend import argmax
 import numpy as np
 from utils import *
 
-
+layer_dim = 128
 pass_len = 10
-good_list, charmap, inv_charmap = load_dataset("../10-million-password-list-top-1000000.txt", pass_len)
+good_list, charmap, inv_charmap = load_dataset("10-million-password-list-top-1000000.txt", pass_len)
 pass_shape = (len(inv_charmap), pass_len)
 
-noise_len = 100
+noise_len = 256
 noise_shape = (noise_len,)
 
 def Conversion(one_dim):
@@ -23,34 +23,63 @@ def Conversion(one_dim):
   return two_dim
 
 
-#Generator Model
-def Generator():
-  model = Sequential()
-  model.add(Flatten(input_shape=noise_shape))
-  model.add(Dense(256))
-  model.add(Dense(np.prod(pass_shape), activation='tanh'))
-  model.add(Reshape(pass_shape))
-  model.add(Softmax(axis=1))
-  model.summary()
+class ResBlock(tf.keras.Model):
+    def __init__(self, dim):
+        super(ResBlock, self).__init__()
+        self.res_block = tf.keras.Sequential([
+            tf.keras.layers.ReLU(True),
+            tf.keras.layers.Conv1D(dim, dim, 5, padding='same'),
+            tf.keras.layers.ReLU(True),
+            tf.keras.layers.Conv1D(dim, dim, 5, padding='same'),
+        ])
 
-  noise = Input(shape=noise_shape)
-  passwords = model(noise)
-  return Model(noise, passwords)
+    def call(self, input, **kwargs):
+        output = self.res_block(input)
+        return input + (0.3 * output)
 
-#Discriminator Model
-def Discriminator():
+def Generator(dim):
     model = Sequential()
-    model.add(Conv1D(filters=5*pass_len, kernel_size=5, activation='relu', input_shape=(len(inv_charmap), 10)))
-    model.add(Dense(256))
-    model.add(LeakyReLU(alpha=0.2))
+    model.add(Dense(256, activation='linear', input_shape=noise_shape))
+    model.add(Reshape((-1, 2, 128)))
+    model.add(ResBlock(dim))
+    model.add(ResBlock(dim))
+    model.add(ResBlock(dim))
+    model.add(ResBlock(dim))
+    model.add(ResBlock(dim))
+    model.add(Reshape((1, 32, 8)))
+    model.add(Conv1D(64, 32, 1, padding='valid'))
     model.add(Flatten())
-    model.add(Dense(1, activation='sigmoid'))
+    model.add(Dense(np.product(pass_shape), activation='linear'))
+    model.add(Softmax(axis=1))
+    model.add(Reshape(pass_shape))
+
+    noise = Input(shape=noise_shape)
+    password = model(noise)
+
+    model.summary()
+    return Model(noise, password)
+
+
+def Discriminator(dim):
+    model = Sequential()
+    model.add(Dense(128, input_shape=pass_shape))
+    model.add(Reshape((3, 32, 128)))
+    model.add(Conv1D(dim, 32, 1, padding='valid'))
+    model.summary()
+    model.add(ResBlock(dim))
+    model.add(ResBlock(dim))
+    model.add(ResBlock(dim))
+    model.add(ResBlock(dim))
+    model.add(ResBlock(dim))
+    model.add(Flatten())
+    model.add(Dense(1, activation='linear'))
+
+    passw = Input(shape=pass_shape)
+    valid = model(passw)
+
     model.summary()
 
-    intput_pass = Input(shape=(len(inv_charmap), 10))
-    validity = model(intput_pass)
-
-    return Model(intput_pass, validity)
+    return Model(passw, valid)
 
 #Saving samples from generator
 def save(epochs):
@@ -69,7 +98,7 @@ def training(epochs, batch_size, sample_interval):
   for epoch in range (0, epochs):
     # Generate a half batch of fake images
     noise = np.random.normal(0, 1, (batch_size, noise_len))
-    gen_imgs = generator.predict(noise)
+    gen_pass = generator.predict(noise)
 
     # #format the output of the generator to train the discriminator
     # bad_list = []
@@ -86,7 +115,7 @@ def training(epochs, batch_size, sample_interval):
 
     #Train discriminator
     d_loss_real = discriminator.train_on_batch(good_list_t, np.ones((batch_size, 1)))
-    d_loss_fake = discriminator.train_on_batch(gen_imgs, np.zeros((batch_size, 1)))
+    d_loss_fake = discriminator.train_on_batch(gen_pass, np.zeros((batch_size, 1)))
     d_loss = 0.5 * np.add(d_loss_real, d_loss_fake)
 
     #Train generator
@@ -100,10 +129,10 @@ def training(epochs, batch_size, sample_interval):
 #Things for ipynb
 optimizer = Adam(0.0002, 0.5)
 
-generator = Generator()
+generator = Generator(layer_dim)
 generator.compile(loss='binary_crossentropy', optimizer=optimizer)
 
-discriminator = Discriminator()
+discriminator = Discriminator(layer_dim)
 discriminator.compile(loss='binary_crossentropy',
     optimizer=optimizer,
     metrics=['accuracy'])
@@ -121,6 +150,4 @@ validity = discriminator(gen_pass)
 combined = Model(z, validity)
 combined.compile(loss='binary_crossentropy', optimizer=optimizer)
 
-noise = np.random.normal(0, 1, (1, 100))
-
-training(10000, 256, 500)
+training(100, 32, 10)
